4ec9780fcdf8f7759f91cb7e25828073feee6c2e
Avery Patterson <ajpatt@ad.unc.edu>
2021-08-31 21:10:24 -0400

    Progress on Exercise 1

--- exercises/ex01/hype_machine.py
+++ exercises/ex01/hype_machine.py
@@ -1,5 +1,6 @@
-# TODO: Write docstring here
-
-# TODO: Initialize __author__ variable here
-
-# TODO: Implement your program logic here
+"""Hype machine."""
+__author__ = "730388679"
+name: str = input("What is your name? ")
+print("Correct ", name + ", you are a queen! ")
+print("Keep up the great work, " + name + ".")
+print("Hope your day just gets better from here, " + name + ".")
---
+++ exercises/ex01/relational_operators.py
@@ -0,0 +1,14 @@
+"Relational operators."
+__author__ = "730388679"
+num1: int = int(input("Pick an integer: "))
+num2: int = int(input("Pick an integer: "))
+answer = num1 < num2
+answer2 = num1 >= num2
+answer3 = num1 == num2
+answer4 = num1 != num2
+print("Left-hand side: " + str(num1))
+print("Right-hand side: " + str(num2))
+print(str(num1) + " < " + str(num2) + " is " + str(answer))
+print(str(num1) + " >= " + str(num2) + " is " + str(answer2))
+print(str(num1) + " == " + str(num2) + " is " + str(answer3))
+print(str(num1) + " != " + str(num2) + " is " + str(answer4))
